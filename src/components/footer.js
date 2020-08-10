f5a4a0d3b3d70b4562a142304c19ad160a8dce90
Lindsay Bloom <lindsay.bloom@sonos.com>
2020-08-09 20:46:13 -0400

    fix footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,29 @@
 import { Link } from "gatsby"
 import React from "react"
-import styled from 'styled-components'
-import { Footer, Anchor } from 'grommet';
-import { Instagram, Twitter, Github, Linkedin } from 'grommet-icons';
+import styled from "styled-components"
+import { Box, Footer as GrommetFooter, Anchor, Text } from "grommet"
+import { Instagram, Twitter, Github, Linkedin } from "grommet-icons"
 
-
-const MyFooter = ({ background }) => (
-  <Footer justify="end" background={background}>
-      <Anchor href="https://www.instagram.com/lindsaybloom/">
-        <Instagram color="accent-2" />
-      </Anchor>
-      <Anchor href="https://www.twitter.com/lbloom97/">
-        <Twitter color="accent-2" />
-      </Anchor>
-      <Anchor href="https://www.github.com/lindsaybloom/">
-        <Github color="accent-2" />
-      </Anchor>
-      <Anchor href="https://www.linkedin.com/in/lindsay-bloom/">
-        <Linkedin color="accent-2" />
-      </Anchor>
-  </Footer>
+const Footer = ({ background }) => (
+  <GrommetFooter background={background} margin={{ top: "xlarge" }}>
+    <Box direction="row" justify="between" width="100%">
+      <Text>Designed &amp; built by Lindsay Bloom </Text>
+      <Box direction="row" justify="between" width="10%">
+        <Anchor href="https://www.instagram.com/lindsaybloom/">
+          <Instagram color="accent-2" />
+        </Anchor>
+        <Anchor href="https://www.twitter.com/lbloom97/">
+          <Twitter color="accent-2" />
+        </Anchor>
+        <Anchor href="https://www.github.com/lindsaybloom/">
+          <Github color="accent-2" />
+        </Anchor>
+        <Anchor href="https://www.linkedin.com/in/lindsay-bloom/">
+          <Linkedin color="accent-2" />
+        </Anchor>
+      </Box>
+    </Box>
+  </GrommetFooter>
 )
 
-export default MyFooter
+export default Footer
